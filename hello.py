9ed99988be486997bcf829bf18885bb053558296
Mehul <aroramehul8@gmail.com>
2019-12-16 02:30:44 -0800

    added person in-chrage category

--- hello.py
+++ hello.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///checklist.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nextiter.db'
 db = SQLAlchemy(app)
 
 class checklist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    assigned_to = db.Column(db.String(50), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
 
     def __repr__(self):
         return str(self.__dict__)
@@ -18,29 +19,31 @@ class checklist(db.Model):
 def index():
 	if request.method == 'POST':
 		task_name = request.form['task']
-		new_task = checklist(content = task_name) 
+		assigned_to = request.form['assigned']
+		new_task = checklist(content = task_name, assigned_to = assigned_to) 
 
 		try:
 			db.session.add(new_task)
 			db.session.commit()
 			return redirect('/')
-
 		except:
-			return "There was an internal issue adding the task"
+			return "Internal Failure"
 		
 	else: 
-		tasks = checklist.query.order_by(checklist.date_created).all()
+		tasks = checklist.query.all()
 		return render_template('index.html', tasks = tasks)
 
 @app.route('/delete/<int:id>')
 def delete_task(id):
 	task_to_delete = checklist.query.get_or_404(id)
+
 	try:
 		db.session.delete(task_to_delete)
 		db.session.commit()
 		return redirect('/')
+
 	except:
-		return "There was a problem deleting your task"
+		return "Internal Failure"
 
 @app.route('/update/<int:id>', methods = ["GET", "POST"])
 def update(id):
@@ -48,20 +51,21 @@ def update(id):
 
 	if request.method == 'POST':
 		task_to_edit.content = request.form['task']
+		task_to_edit.assigned_to = request.form['assigned']
 
 		try:
 			db.session.commit()
 			return redirect('/')
 
 		except:
-			return "There was an internal issue updating the task"
+			return "Internal error"
 
 	else:
 		return render_template('update.html', task = task_to_edit)
 
 @app.route('/gethc')
 def write():
-	tasks = checklist.query.order_by(checklist.date_created).all()
+	tasks = checklist.query.all()
 	with open("checklist.txt", "w") as f:
 		for task in tasks:
 			f.write(task.__repr__())
